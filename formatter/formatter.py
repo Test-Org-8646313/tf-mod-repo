0fc6b4fa3a50623ebdccd3890e3eeab7f6197e5e
Vinod Mandavkar <maker.vinodm@gmail.com>
2024-05-31 21:48:00 +0530

    Added v2

--- formatter/formatter.py
+++ formatter/formatter.py
@@ -1,8 +1,8 @@
 def parse_json_data(data):
     try:
-        print("---------Version 1-------\n")
-        print(f"Organization: {data['organization']}")
-        print(f"Total Employees: {data['employees']}\n")
+        print("---------Version 2-------\n")
+        print(f"Organization Name: {data['organization']}")
+        print(f"Total Employee Count: {data['employees']}\n")
         print("---------------------------")
     except Exception as error:
         print("Error Occurred: ",error)
